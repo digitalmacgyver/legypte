badeb940363f709bfad5a39c7781cbdf96db80c8
Matt Hayward <mjhayward+legypte@gmail.com>
2013-06-10 20:13:41 -0700

    Trying to commit static files.

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, url
+from django.conf import settings
 
 from gallery import views
 
 urlpatterns = patterns( '',
                         url( r'^gallery\.js$', views.gallery, name='gallery' ),
                         url( r'^flickruser/(?P<user>[\w]+)/$', views.user, name='user' ),
+                        url( r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
                         url( r'^$', views.display, name='display' ),
                         )
 

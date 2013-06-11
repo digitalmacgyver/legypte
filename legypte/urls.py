d11e45cb74f505ac289fad2473aa9e0474a84e8e
Matt Hayward <mjhayward+legypte@gmail.com>
2013-06-10 19:51:33 -0700

    Set up basic app.

--- legypte/urls.py
+++ legypte/urls.py
@@ -14,4 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+#                       url( r'^$', include( 'gallery.urls', namespace='gallery' ) ),
+                       url( r'', include( 'gallery.urls', namespace='gallery' ) ),
 )

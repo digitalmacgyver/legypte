bfba280ab92d0e3c202244687c5055dfbfbc0116
Matt Hayward <mjhayward+legypte@gmail.com>
2013-07-27 00:34:17 -0700

    test

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,6 +3,8 @@ from django.conf import settings
 
 from gallery import views
 
+
+
 urlpatterns = patterns( '',
                         url( r'^gallery\.js$', views.gallery, name='gallery' ),
                         url( r'^flickr/(?P<user>[\w]+)/gallery\.js$', views.user_gallery, name='user' ),
